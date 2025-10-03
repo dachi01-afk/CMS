a4eccec38a8d617c2a29bc3b7723359caf568b4b
Davidgultom <DavidgultomM@gmail.com>
2025-10-03 16:26:50 +0700

    Membuat Function Create Pasien -> David

--- app/Http/Controllers/Admin/PasienController.php
+++ app/Http/Controllers/Admin/PasienController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Pasien;
 use Illuminate\Http\Request;
 
 class PasienController extends Controller
 {
     public function index()
     {
+        $dataPasien = Pasien::all();
+
         return view('admin.pasien');
+    }
+
+    public function createPasien(Request $request)
+    {
+
+        $request->validate([
+            'nama_pasien' => ['required'],
+            'alamat' => ['required'],
+            'tanggal_lahir' => ['required', 'date'],
+        ]);
+
+        Pasien::created([
+            'nama_pasien' => $request->
+        ]);
+
+        return $request;
     }
 }
