332f4518e45bd3e9f3a6b2d38d3262876513957d
Davidgultom <DavidgultomM@gmail.com>
2025-10-07 14:17:21 +0700

    Git Pull

---
+++ app/Http/Controllers/Testing/TestingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Testing;
+
+use App\Http\Controllers\Controller;
+use App\Models\JadwalDokter;
+use Illuminate\Http\Request;
+
+class TestingController extends Controller
+{
+    public function index() {
+        
+
+        return view('testing.index');
+    }
+}
---
+++ resources/views/testing/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    
+</head>
+
+<body>
+    <div class="max-w-7x mx-auto my-5">
+        <div class="grid gap-4">
+            <div class="grid gap-2">
+                <label class="">Masukkan Nama</label>
+                <input type="text">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
