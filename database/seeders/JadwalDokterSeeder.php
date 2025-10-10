313c1373302fa8d0fae3657990cd2c30392b2292
itsmeFer <ferdinandsianturi28@gmail.com>
2025-10-10 14:46:25 +0700

    push new seed

--- database/seeders/JadwalDokterSeeder.php
+++ database/seeders/JadwalDokterSeeder.php
@@ -44,9 +44,9 @@ class JadwalDokterSeeder extends Seeder
 
         JadwalDokter::create([
             'dokter_id' => 1,
-            'hari' => 'Senin',
+            'hari' => 'Jumat',
             'jam_awal' => '08:00:00',
-            'jam_selesai' => '15:00:00',
+            'jam_selesai' => '18:00:00',
         ]);
 
         JadwalDokter::create([
