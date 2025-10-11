d0061c25ec8b784e70e0a17266c85cca6bd0ecba
Davidgultom <DavidgultomM@gmail.com>
2025-10-11 13:31:50 +0700

    push

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,34 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                "resources/css/app.css",
+                "resources/js/app.js",
 
                 // admin
-                'resources/js/admin/dashboard.js',
-                
-                // manajemen-pengguna
-                'resources/js/admin/manajemenPengguna/data_pengguna.js',
-                'resources/js/admin/manajemenPengguna/data_dokter.js',
-                'resources/js/admin/manajemenPengguna/data_apoteker.js',
-                'resources/js/admin/manajemenPengguna/data_pasien.js',
+                "resources/js/admin/dashboard.js",
 
-		// pengaturan klinik
-		'resources/js/admin/pengaturanKlinik/jadwal_dokter.js',
-		'resources/js/admin/pengaturanKlinik/daftar_obat.js',
-		'resources/js/admin/laporan/laporan_kunjungan.js',
-		'resources/js/admin/laporan/laporan_keuangan.js',
-		'resources/js/admin/laporan/laporan_resep_dan_apotek.js',
-		'resources/js/admin/laporan/laporan_administrasi.js',
-		'resources/js/admin/dataMedisPasien/rekam_medis_elektronik.js',
-		'resources/js/admin/dataMedisPasien/data_diagnosa_dan_konsultasi.js',
-		'resources/js/admin/dataMedisPasien/data_hasil_lab.js',
-		
-             ],
+                // manajemen-pengguna
+                "resources/js/admin/manajemenPengguna/data_pengguna.js",
+                "resources/js/admin/manajemenPengguna/data_dokter.js",
+                "resources/js/admin/manajemenPengguna/data_apoteker.js",
+                "resources/js/admin/manajemenPengguna/data_pasien.js",
+                "resources/js/admin/pengambilanObat/pengambilan_obat.js",
+
+                // pengaturan klinik
+                "resources/js/admin/pengaturanKlinik/jadwal_dokter.js",
+                "resources/js/admin/pengaturanKlinik/daftar_obat.js",
+                "resources/js/admin/laporan/laporan_kunjungan.js",
+                "resources/js/admin/laporan/laporan_keuangan.js",
+                "resources/js/admin/laporan/laporan_resep_dan_apotek.js",
+                "resources/js/admin/laporan/laporan_administrasi.js",
+                "resources/js/admin/dataMedisPasien/rekam_medis_elektronik.js",
+                "resources/js/admin/dataMedisPasien/data_diagnosa_dan_konsultasi.js",
+                "resources/js/admin/dataMedisPasien/data_hasil_lab.js",
+            ],
             refresh: true,
         }),
     ],
