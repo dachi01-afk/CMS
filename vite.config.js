59cdd500cbde4947563e3b55c379183d2b7e5b44
Davidgultom <DavidgultomM@gmail.com>
2025-11-05 23:54:02 +0700

    Menambahkan tag script js nya data_kasir ke dalam vite.config

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,7 @@ export default defineConfig({
                 "resources/js/admin/manajemenPengguna/data_pengguna.js",
                 "resources/js/admin/manajemenPengguna/data_dokter.js",
                 "resources/js/admin/manajemenPengguna/data_apoteker.js",
+                "resources/js/admin/manajemenPengguna/data_kasir.js",
                 "resources/js/admin/manajemenPengguna/data_pasien.js",
                 "resources/js/admin/pengambilanObat/pengambilan_obat.js",
                 "resources/js/admin/jadwalKunjungan/jadwal_kunjungan.js",
