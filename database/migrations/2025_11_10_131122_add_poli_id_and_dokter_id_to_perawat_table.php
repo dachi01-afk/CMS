a62f13271b93d9024af1925f53f1cb063f00d1a3
Davidgultom <DavidgultomM@gmail.com>
2025-11-10 13:50:39 +0700

    Menambahkan Relasi Pada Tabel Perawat. Yaitu Menambahkan Field poli_id dan dokter_id

---
+++ database/migrations/2025_11_10_131122_add_poli_id_and_dokter_id_to_perawat_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('perawat', function (Blueprint $table) {
+            // Tambah kolom baru (nullable dulu agar aman untuk existing rows)
+            $table->foreignId('poli_id')
+                ->nullable()
+                ->after('user_id')->constrained('poli', 'id', 'perawat_poli_id')->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->foreignId('dokter_id')
+                ->nullable()
+                ->after('poli_id')->constrained('dokter', 'id', 'perawat_dokter_id')->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('perawat', function (Blueprint $table) {
+            // Drop FK lalu kolomnya
+            $table->dropForeign('perawat_poli_id');
+            $table->dropForeign('perawat_dokter_id');
+
+            $table->dropColumn(['poli_id', 'dokter_id']);
+        });
+    }
+};
