1fb8a87b27d02f1dca7e8d23f026faee74a298c6
Davidgultom <DavidgultomM@gmail.com>
2025-11-10 09:49:18 +0700

    Memperbaiki Routeing Pada Role Kasir

--- resources/views/kasir/pembayaran/transaksi.blade.php
+++ resources/views/kasir/pembayaran/transaksi.blade.php
@@ -110,7 +110,7 @@
                         </div>
 
                         <div class="gap-4 sm:flex sm:items-center">
-                            <a href="{{ route('kasir.index') }}"
+                            <a href="{{ route('kasir.dashboard') }}"
                                 class="w-full rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                                 Kembali ke halaman kasir
                             </a>
