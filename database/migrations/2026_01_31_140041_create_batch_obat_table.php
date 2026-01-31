c747b0df3379c33361c55c823cd331411165770e
Davidgultom <DavidgultomM@gmail.com>
2026-01-31 14:17:37 +0700

    Memperbaiki nama file migrasi batch_obat

--- database/migrations/2026_01_31_140041_create_batch_obat_table.php
+++ database/migrations/2026_01_31_140041_create_batch_obat_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('batch_obats', function (Blueprint $table) {
+        Schema::create('batch_obat', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('batch_obats');
+        Schema::dropIfExists('batch_obat');
     }
 };
