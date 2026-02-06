64243129d5e8edebb2b9cd134c8893df88df89e1
itsmeFer <ferdinandsianturi28@gmail.com>
2026-02-06 23:20:48 +0700

    Menambahkan fitur notifikasi si controller perawat dan fcm token

--- database/migrations/2026_01_09_120756_create_layanan_polis_table.php
+++ database/migrations/2026_01_09_120756_create_layanan_polis_table.php
@@ -11,18 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('layanan_poli')) {
+            return; 
+        }
+
         Schema::create('layanan_poli', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('layanan_id')->nullable()->constrained(
-                'layanan',
-                'id',
-                'layanan_poli_layanan_id'
-            )->cascadeOnUpdate()->cascadeOnDelete();
-            $table->foreignId('poli_id')->nullable()->constrained(
-                'poli',
-                'id',
-                'layanan_poli_poli_id'
-            )->cascadeOnUpdate()->cascadeOnDelete();
+
+            $table->foreignId('layanan_id')->nullable()
+                ->constrained('layanan')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->foreignId('poli_id')->nullable()
+                ->constrained('poli')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
             $table->timestamps();
         });
     }
